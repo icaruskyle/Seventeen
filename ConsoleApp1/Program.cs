9db7cb1e376cc53e86bf73f3ae5c8d2e56e3a7b3
Aldrin Kyle Almenanza <aldrinkylebalmenanza@iskolarngbayan.pup.edu.ph>
2024-11-17 19:11:02 +0800

    email

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+
+
+namespace new_email_tool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Say the Name Seventeen!!", "saythename@gmail.com"));
+            message.To.Add(new MailboxAddress("User", "aaldrinkyle@gmail.com"));
+            message.Subject = "Thanks for Subscribing!";
+
+            message.Body = new TextPart("html")
+            {
+                Text = "<h1>Hi, User!</h1>" +
+                "<p>Thank you for subscribing to Carat Membership</p>" +
+                "<p><strong>Welcome!</strong></p>"
+            };
+
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    client.Connect("sandbox.smtp.mailtrap.io", 2525, MailKit.Security.SecureSocketOptions.StartTls);
+
+                    client.Authenticate("fff467d51f5c6f", "1d446921bf8d05");
+
+                    client.Send(message);
+                    Console.WriteLine("Email sent successfully through Mailtrap.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error sending email: {ex.Message}");
+                }
+                finally
+                {
+                    client.Disconnect(true);
+                }
+            }
+        }
+    }
+}
---
+++ SvtBL/Emailservice.cs
@@ -0,0 +1,47 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+
+namespace new_email_tool
+{
+    public class EmailService
+    {
+        string _smtpServer = "sandbox.smtp.mailtrap.io";
+        int _port = 2525;
+        string _userName = "fff467d51f5c6f";
+        string _password = "1d446921bf8d05";
+        string _recipientName = "Aldrin Kyle Almenanza";
+        string _recipientEmail = "aaldrinkyle@gmail.com";
+        public void SendEmail(string subject, string htmlBody)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Hello Say The Name Seventeen", "saythename@gmail.com"));
+            message.To.Add(new MailboxAddress(_recipientName, _recipientEmail));
+            message.Subject = subject;
+
+            message.Body = new TextPart("html")
+            {
+                Text = htmlBody
+            };
+
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    client.Connect(_smtpServer, _port, MailKit.Security.SecureSocketOptions.StartTls);
+                    client.Authenticate(_userName, _password);
+                    client.Send(message);
+                    Console.WriteLine("Email sent successfully.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error sending email: {ex.Message}");
+                }
+                finally
+                {
+                    client.Disconnect(true);
+                }
+            }
+        }
+    }
+}
